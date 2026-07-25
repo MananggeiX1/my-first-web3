bd31d620d5e95b5f864c83b57b8052fa96fe3063
Mananggei <35006950+MananggeiX1@users.noreply.github.com>
2026-07-25 09:06:43 +0700

    Update app.js

--- frontend/app.js
+++ frontend/app.js
@@ -1,5 +1,38 @@
-async function connectWallet(){
+const contractAddress = "PASTE_ALAMAT_KONTRAK_DI_SINI";
 
-alert("Wallet Connected");
+const abi = [
+  "function setValue(uint256 _value) public",
+  "function getValue() public view returns (uint256)"
+];
 
+async function connect() {
+  if (typeof window.ethereum !== "undefined") {
+    await window.ethereum.request({ method: "eth_requestAccounts" });
+
+    const provider = new ethers.BrowserProvider(window.ethereum);
+    const signer = await provider.getSigner();
+
+    const contract = new ethers.Contract(contractAddress, abi, signer);
+
+    return contract;
+  } else {
+    alert("Silakan install MetaMask.");
+  }
+}
+
+async function setValue() {
+  const contract = await connect();
+  const value = document.getElementById("valueInput").value;
+
+  const tx = await contract.setValue(value);
+  await tx.wait();
+
+  alert("Nilai berhasil disimpan!");
+}
+
+async function getValue() {
+  const contract = await connect();
+  const value = await contract.getValue();
+
+  document.getElementById("result").innerText = value;
 }
