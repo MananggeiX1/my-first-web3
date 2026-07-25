eefcd4e79d91cb37db84e93d8fd4667eec20006d
Mananggei <35006950+MananggeiX1@users.noreply.github.com>
2026-07-25 09:04:54 +0700

    Update SimpleStorage.sol

--- contracts/SimpleStorage.sol
+++ contracts/SimpleStorage.sol
@@ -3,14 +3,40 @@ pragma solidity ^0.8.20;
 
 contract SimpleStorage {
 
-uint256 private value;  
+    uint256 private value;
+    address public owner;
 
-function setValue(uint256 _value) public {  
-    value = _value;  
-}  
+    event ValueUpdated(uint256 oldValue, uint256 newValue);
 
-function getValue() public view returns(uint256){  
-    return value;  
-}
+    constructor() {
+        owner = msg.sender;
+    }
 
+    function setValue(uint256 _value) public {
+        uint256 oldValue = value;
+        value = _value;
+
+        emit ValueUpdated(oldValue, _value);
+    }
+
+    function getValue() public view returns (uint256) {
+        return value;
+    }
+
+    function increment() public {
+        value += 1;
+    }
+
+    function decrement() public {
+        require(value > 0, "Value cannot be negative");
+        value -= 1;
+    }
+
+    function reset() public {
+        value = 0;
+    }
+
+    function getOwner() public view returns (address) {
+        return owner;
+    }
 }
